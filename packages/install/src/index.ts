3173dfb0f39545c018957bc7b720a1ef80ca32c0
Maciej Ziehlke <mac@dmio.co>
2026-06-28 16:54:54 +0100

    Adds logger to install

--- packages/install/src/index.ts
+++ packages/install/src/index.ts
@@ -1,14 +1,19 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 import { serve } from "@hono/node-server";
 import { readFile } from "node:fs/promises";
 import { join } from "node:path";
 
 const app = new Hono();
+app.use(logger());
 
 app.get("/", (c) => c.redirect("https://github.com/digitalmio/patiom"));
 
 app.get("/setup.sh", async (_c) => {
-  const content = await readFile(join(import.meta.dirname, "setup.sh"), "utf-8");
+  const content = await readFile(
+    join(import.meta.dirname, "setup.sh"),
+    "utf-8",
+  );
   return new Response(content, {
     headers: {
       "Content-Type": "text/x-shellscript; charset=utf-8",
